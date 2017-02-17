5e6c650d30af6f7d8cacfa9b1992bfa3ac502d8f
Rafael <rapachecobeiro@hotmail.com>
2017-02-17 09:59:49 -0200

    Initial draft

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,7 +3,52 @@ let attempt = document.getElementById('attempt');
 
 function guess() {
     let input = document.getElementById('user-guess');
-    //add functionality to guess function here
+    if (answer == "" && attempt == "") {
+      setHiddenFields();
+    }
+    if (!validateInput(input.value)) {
+      return false;
+    } else {
+      attempt++;
+    }
 }
 
-//implement new functions here
+function setHiddenFields() {
+  answer = Math.random() * 10000;
+  answer = Math.floor(answer);
+  answer = answer.toString();
+  if (answer.length != 4) {
+    while (answer.length < 4) {
+      answer = "0" + answer;
+    }
+  }
+  attempt = 0;
+}
+
+function setMessage(mes) {
+  document.getElementById('message').innerHTML = mes;
+}
+
+function validateInput(guess) {
+  if (guess.length == 4) {
+    return true;
+  } else {
+    setMessage("Guesses must be exactly 4 characters long.");
+    return false;
+  }
+}
+
+function getResults(input) {
+  result = "<div class="row"><span class="col-md-6">" + input + "</span><div class="col-md-6">";
+  for (var i = 0; i < 4; i++) {
+    if (input[i] == answer[i]) {
+      result += "<span class="glyphicon glyphicon-ok"></span>";
+    } else if (answer.indexOf(input[i]) != -1) {
+      result += "<span class="glyphicon glyphicon-transfer"></span>";
+    } else {
+      result += "<span class="glyphicon glyphicon-remove"></span>";
+    }
+  }
+  result += "</div></div>";
+  document.getElementById('results').innerHTML = result;
+}
